2f344066bc7a2506121ec469186dae56bf7df592
Sukhwinder Singh <singh@sukhi.codes>
2021-03-16 15:19:30 -0700

    import 'dart:math'; -> Random numbers !!! Implemented Stateful Widget to change dice images with click. Wrote a dart function as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,20 +16,53 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void updateDice() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          flex: 1,
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  updateDice();
+                });
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  updateDice();
+                });
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
